b66bf2f7e7f2bd626720cf58420e2ef58af987ac
basON <wch.basinski@gmail.com>
2024-03-20 00:39:06 +0100

    Add rating methods

--- Backend/src/main/java/com/example/backend/service/RatingService.java
+++ Backend/src/main/java/com/example/backend/service/RatingService.java
@@ -1,4 +1,16 @@
 package com.example.backend.service;
 
+import com.example.backend.dtos.Payment.UpdatePaymentRequest;
+import com.example.backend.dtos.Rating.AddRatingRequest;
+import com.example.backend.dtos.Rating.RatingFilter;
+import com.example.backend.dtos.Rating.RatingResponse;
+
+import java.util.List;
+
 public interface RatingService {
+    RatingResponse addRating(AddRatingRequest addRatingRequest);
+    RatingResponse getRating(String id);
+    RatingResponse updateRating(UpdatePaymentRequest updatePaymentRequest);
+
+    List<RatingResponse> getRatings(RatingFilter ratingFilter);
 }
--- Backend/src/main/java/com/example/backend/service/impl/RatingServiceImpl.java
+++ Backend/src/main/java/com/example/backend/service/impl/RatingServiceImpl.java
@@ -1,8 +1,33 @@
 package com.example.backend.service.impl;
 
+import com.example.backend.dtos.Payment.UpdatePaymentRequest;
+import com.example.backend.dtos.Rating.AddRatingRequest;
+import com.example.backend.dtos.Rating.RatingFilter;
+import com.example.backend.dtos.Rating.RatingResponse;
 import com.example.backend.service.RatingService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RatingServiceImpl implements RatingService {
+    @Override
+    public RatingResponse addRating(AddRatingRequest addRatingRequest) {
+        return null;
+    }
+
+    @Override
+    public RatingResponse getRating(String id) {
+        return null;
+    }
+
+    @Override
+    public RatingResponse updateRating(UpdatePaymentRequest updatePaymentRequest) {
+        return null;
+    }
+
+    @Override
+    public List<RatingResponse> getRatings(RatingFilter ratingFilter) {
+        return null;
+    }
 }
