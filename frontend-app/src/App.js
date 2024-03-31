84bb154194843e9ab112f6f08589b33b695ee2ee
osman <osmi4aliyev@gmail.com>
2024-03-31 15:34:46 +0200

    finish login page without animation

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,11 +1,24 @@
 import WordCard from './WordCard';
 import LoginForm from './LoginForm';
+import {useState} from 'react';
 
 function App() {
+  const [isRegister,setIsRegister]=useState(false);
+  const onRegister=()=>{
+    setIsRegister(true);
+  }
+  const onLogin=()=>{
+    setIsRegister(false);
+  }
+
   return (
   <div class ="forms">
-    <LoginForm/>
-    <WordCard/>
+    {!isRegister && 
+    <><LoginForm isLogin={true}/> 
+    <WordCard onClick={onRegister} isLogin={true}/></>}
+    {isRegister && 
+    <><WordCard onClick={onLogin} isLogin={false}/>
+    <LoginForm isLogin={false}/></>}
   </div>
   );
 }
--- frontend-app/src/LoginForm.js
+++ frontend-app/src/LoginForm.js
@@ -1,28 +1,33 @@
-function LoginForm(){
-    return (
-    <div class="wrapper">
-        <form action="">
-            <h1>Login</h1>
+const LoginForm=({isLogin})=>{
+  return (
+  <div class="wrapper">
+  <form action="">
+    {isLogin && <h1>Login</h1>}
+    {!isLogin && <h1>Register</h1>}
+  <div class = "input-box">
+    <input type="text" placeholder="Username" required/>
+    <i class='bx bxs-user'/>
+  </div>
 
-    <div class = "input-box">
-      <input type="text" placeholder="Username" required/>
-      <i class='bx bxs-user'/>
+  <div class = "input-box">
+    <input type="password" placeholder="Password" required/>
+    <i class='bx bxs-lock-alt'/>
+  </div>
+  {
+    !isLogin && <div class = "input-box">
+    <input type="password" placeholder="Confirm Password" required/>
+    <i class='bx bxs-lock-alt'/>
     </div>
+  }
+  {isLogin && <div class = "remember-forgot">
+    <label><input type = "checkbox"/>Remember me</label>
+    <a href="#">Forgot password?</a>
+  </div>}
 
-    <div class = "input-box">
-      <input type="password" placeholder="Password" required/>
-      <i class='bx bxs-lock-alt'/>
-    </div>
-
-    <div class = "remember-forgot">
-      <label><input type = "checkbox"/>Remember me</label>
-      <a href="#">Forgot password?</a>
-    </div>
-
-    <button type="submit" class="btn">Login</button>
-
-        </form>
-    </div>
+  {isLogin && <button type="submit" class="btn">Login</button>}
+  {!isLogin && <button type="submit" class="btninv">Register</button>}
+  </form>
+  </div>
     )
 }
 
