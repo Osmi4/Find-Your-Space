623ec6557dc5ce3cbd6e76461b5770c502dc063a
basON <wch.basinski@gmail.com>
2024-06-04 23:58:39 +0200

    edit image flow

--- Backend/src/main/java/com/example/backend/repository/ImageRepository.java
+++ Backend/src/main/java/com/example/backend/repository/ImageRepository.java
@@ -3,6 +3,9 @@ package com.example.backend.repository;
 import com.example.backend.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, String> {
 
@@ -11,5 +14,13 @@ public interface ImageRepository extends JpaRepository<Image, String> {
 
     @Query("select i from Image i where i.space.spaceId = ?1")
     Image findBySpaceId(String spaceId);
+
+    @Query("delete from Image i where i.imageId = ?1")
+    int deleteByImageId(String imageId);
+    //findBySpace_SpaceId -> return a list of images
+    @Query("select i from Image i where i.space.spaceId = ?1")
+    Page<Image> findBySpace_SpaceId(String spaceId);
+
+
 
 }
