27e694e1696cd537639d80f0979df874f5008768
osman <osmi4aliyev@gmail.com>
2024-06-08 23:21:30 +0200

    admin panel design fixed

--- frontend-admin/src/components/Nav.js
+++ frontend-admin/src/components/Nav.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Navbar, NavbarContent, Button, Input , Dropdown, DropdownItem, DropdownMenu, DropdownTrigger} from "@nextui-org/react";
+import { Navbar, NavbarContent, Button, Input , Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Avatar, NavbarBrand} from "@nextui-org/react";
+import logo from "../images/logo.png";
 import axios from "axios";
 
 const Nav = () => {
-    const { loginWithRedirect, getIdTokenClaims, isAuthenticated, logout } = useAuth0();
+    const { loginWithRedirect, getIdTokenClaims, isAuthenticated, logout, user } = useAuth0();
     const [formData, setFormData] = useState({ email: '', password: '' });
     const [token, setToken] = useState("");
     const [isAdmin, setIsAdmin] = useState(false);
@@ -39,12 +40,16 @@ const Nav = () => {
         }
     }, [isAuthenticated, getIdTokenClaims, token]);
 
+    const handleLogout = () => {
+        logout({ returnTo: window.location.origin });
+    };
+
     return (
-        <Navbar isBordered className='border-black justify-between' maxWidth="full">
-            <NavbarContent className="sm:flex" justify="between">
-                <Dropdown className='sm:hidden'>
+        <Navbar isBordered className='border-black' maxWidth="full">
+            <NavbarContent className="lg:flex lg:justify-center">
+                <Dropdown className='lg:hidden'>
                     <DropdownTrigger className='lg:hidden'>
-                        <div className='flex flex-col gap-y-[3px]'>
+                        <div className='flex flex-col gap-y-[3px] lg:hidden'>
                             <hr className='w-[20px] border-black border-1'></hr>
                             <hr className='w-[20px] border-black border-1'></hr>
                             <hr className='w-[20px] border-black border-1'></hr>
@@ -80,7 +85,12 @@ const Nav = () => {
                         </DropdownItem>
                     </DropdownMenu>
                 </Dropdown>
-                <div className='gap-[1vw] hidden lg:flex'>
+                <div className='hidden lg:flex gap-8 items-center'>
+                <NavbarBrand className='lg:mr-[20px] hidden lg:block'>
+                    <Link to="/">
+                        <img src={logo} className="w-[48px] h-[48px]" alt="" />
+                    </Link>
+                </NavbarBrand>
                     {isAdmin && <Link to="/admin">Admin</Link>}
                     <Link to="/" className='font-bold'>Home</Link>
                     <Link to="/reports">Reports</Link>
@@ -90,15 +100,23 @@ const Nav = () => {
                     <Link to="/bookings">Bookings</Link>
                     <Link to="/message">Messages</Link>
                 </div>
-
-                {isAuthenticated ? (
-                    <Button onClick={() => logout({ returnTo: window.location.origin })}>Logout</Button>
-                ) : (
+                <NavbarContent justify='end'>
+                {!isAuthenticated && (
                     <Button onClick={loginWithRedirect}>Login</Button>
                 )}
+                </NavbarContent>
             </NavbarContent>
             {isAuthenticated && (
-                <div>You are Logged in</div>
+                <Dropdown>
+                <DropdownTrigger>
+                    <Avatar src={user.picture} as="button"/>
+                </DropdownTrigger>
+                <DropdownMenu>
+                    <DropdownItem key="logout" color="error" onClick={handleLogout}>
+                        Logout
+                    </DropdownItem>
+                </DropdownMenu>
+                </Dropdown>
             )}
         </Navbar>
     );
